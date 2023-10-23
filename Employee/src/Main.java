1a33688e8bc8015c5c9950b183ae2fb0fb1f3dd9
Eilwarin <jamariferguson@tuta.io>
2023-10-23 16:46:54 -0500

    Changed output string

--- Employee/src/Main.java
+++ Employee/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter essageeeee: ");
+            System.out.print("Enter message: ");
             String message = input.nextLine();
             System.out.println(message);
         }
