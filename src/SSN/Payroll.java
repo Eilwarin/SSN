ce56dc157de905b220f0648da0cc41c91e100241
Eilwarin <jamariferguson@tuta.io>
2023-11-20 20:41:06 -0500

    Ready for upload

--- src/SSN/Payroll.java
+++ src/SSN/Payroll.java
@@ -1,5 +1,10 @@
+// File: Payroll.java
+// Authors: Jamari Ferguson, Dontray Blackwood, Rajaire Thomas, Alexi Brooks, Rochelle Gordon
+
+// Package declaration for the Payroll class under the SSN package
 package SSN;
 
+// Import statements for necessary Java classes and libraries
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,9 +12,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
 import java.util.Random;
-import java.util.Scanner;
 
-public class Payroll extends EmployeeTax{
+// Payroll class extending EmployeeTax
+public class Payroll extends EmployeeTax {
+    // Instance variables
     protected float hoursWorked;
     protected double regularPay;
     protected double overtimePay;
@@ -21,34 +27,37 @@ public class Payroll extends EmployeeTax{
     protected LocalDate processedDate;
     protected String chequeNumber;
 
-    public Payroll(){}
+    // Default constructor
+    public Payroll() {}
 
+    // Method to calculate payroll for employees
     public List<Payroll> calculatePay() {
         List<Payroll> newPayroll = new ArrayList<>();
         StringBuilder chequeNumber = new StringBuilder();
         Random seed = new Random();
         double overtime = 0;
 
+        // Check for overtime
         if (getHoursWorked() > 40.0f) {
             overtime = getHoursWorked() - 40.0f;
         }
-        for (int ch = 0; ch < 4; ch++){
+
+        // Generate a random cheque number
+        for (int ch = 0; ch < 4; ch++) {
             int asciiAlpha = 65 + seed.nextInt(90 - 65 + 1);
             int asciiNumeric = 48 + seed.nextInt(57 - 48 + 1);
-
             chequeNumber.append((char) asciiAlpha);
             chequeNumber.append((char) asciiNumeric);
         }
 
+        // Calculate various pay components
         setRegularPay((getHoursWorked() - overtime) * getPositionRegRate());
-
         setOvertimePay(overtime * getPositionOtRate());
-
         setGrossPay(getRegularPay() + getOvertimePay());
-
         setIncomeTaxable(getGrossPay() > getIncomeThreshold());
 
-        if(isIncomeTaxable()){
+        // Calculate taxes and net pay if income is taxable
+        if (isIncomeTaxable()) {
             setTaxableIncome(getGrossPay() - getIncomeThreshold());
             setIncomeTax(getTaxableIncome() * getIncomeTaxRate());
             setNisTax(getTaxableIncome() * getNisTaxRate());
@@ -59,30 +68,31 @@ public class Payroll extends EmployeeTax{
             setPaidEduTax(getEduTax() + getIncomeTax());
         }
 
+        // Set processed date, cheque number, and add to the payroll list
         setProcessedDate(LocalDate.now());
-
         setChequeNumber(chequeNumber.toString());
-
         newPayroll.add(this);
 
         return newPayroll;
     }
 
-    public void payrollFileProcessing(List<Payroll> payrolls, Path path, boolean ratesRegistered){
+    // Method for processing payroll and writing to a file
+    public void payrollFileProcessing(List<Payroll> payrolls, Path path, boolean ratesRegistered) {
         try {
+            // Check if the file exists, if not, create and write the header
             if (!Files.exists(path)) {
                 BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path.toFile()));
-
                 bufferedWriter.write("Date\tCheque No.\tID No.\tF. Name\tL. Name\tDept. Code\tPosition\tHrs. Worked\t" +
                         "Reg. Pay\tOT. Pay\tGross Pay\tNet Pay\tIncome Tax\tNIS Tax\tEducation Tax");
                 bufferedWriter.newLine();
                 bufferedWriter.close();
             }
 
-            if (ratesRegistered){
+            // If rates are registered, append payroll information to the file
+            if (ratesRegistered) {
                 BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path.toFile(), true));
 
-                for (Payroll ignored : payrolls){
+                for (Payroll ignored : payrolls) {
                     bufferedWriter.write(getProcessedDate() + "\t" + getChequeNumber() + "\t" +
                             getIdNumber() + "\t" + getFirstName() + "\t" + getLastName() + "\t" +
                             getDepartmentCode() + "\t" + getPosition() + "\t" + getHoursWorked() +
@@ -92,30 +102,35 @@ public class Payroll extends EmployeeTax{
                 }
                 bufferedWriter.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error occurred " + e);
         }
     }
 
-    public boolean viewEmployeePayroll(Path path){
+    // Method to view an employee's payroll information from a file
+    public boolean viewEmployeePayroll(Path path) {
         boolean isPaid = false;
-        try{
+        try {
+            // Check if the file exists
             if (Files.exists(path)) {
                 BufferedReader bufferedReader = new BufferedReader(new FileReader(path.toFile()));
                 String line;
                 boolean headerSkipped = false;
                 String[] fileContent;
 
-
+                // Read each line from the file
                 while ((line = bufferedReader.readLine()) != null) {
+                    // Skip the header
                     if (!headerSkipped) {
                         headerSkipped = true;
                         continue;
                     }
                     fileContent = line.split("\t");
 
+                    // Check if the line contains information for the requested employee
                     if (fileContent.length == 15 && fileContent[2].equals(getIdNumber())) {
                         isPaid = true;
+                        // Set payroll information
                         setProcessedDate(LocalDate.parse(fileContent[0]));
                         setChequeNumber(fileContent[1]);
                         setFirstName(fileContent[3]);
@@ -134,67 +149,77 @@ public class Payroll extends EmployeeTax{
                 }
                 bufferedReader.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error has occurred. " + e);
         }
 
         return isPaid;
     }
-    public boolean getPositionRates(){
+
+    // Method to retrieve position rates from a file
+    public boolean getPositionRates() {
         boolean ratesRegistered = false;
         try {
-            if (Files.exists(Path.of("rates.txt"))){
+            // Check if the rates file exists
+            if (Files.exists(Path.of("rates.txt"))) {
                 BufferedReader reader = new BufferedReader(new FileReader("rates.txt"));
                 String line;
                 boolean headerSkipped = false;
 
-                while((line = reader.readLine()) != null){
-                    if (!headerSkipped){
+                // Read each line from the rates file
+                while ((line = reader.readLine()) != null) {
+                    if (!headerSkipped) {
                         headerSkipped = true;
                         continue;
                     }
                     String[] fileContent = line.split("\t");
 
-                    if (fileContent.length == 5 && fileContent[1].equals(getIdNumber())){
+                    // Check if the line contains information for the requested employee
+                    if (fileContent.length == 5 && fileContent[1].equals(getIdNumber())) {
                         ratesRegistered = true;
                         setPositionRegRate(Double.parseDouble(fileContent[3]));
                         setPositionOtRate(Double.parseDouble(fileContent[4]));
                     }
                 }
-
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error has occurred.");
         }
         return ratesRegistered;
     }
 
-    public boolean getTaxInfo(){
+    // Method to retrieve tax information from a file
+    public boolean getTaxInfo() {
         boolean taxRegistered = false;
         try {
-            if (Files.exists(Path.of("tax_info.txt"))){
+            // Check if the tax info file exists
+            if (Files.exists(Path.of("tax_info.txt"))) {
                 BufferedReader reader = new BufferedReader(new FileReader("tax_info.txt"));
                 String line;
                 boolean headerSkipped = false;
 
-                while((line = reader.readLine()) != null){
-                    if (!headerSkipped){
+                // Read each line from the tax info file
+                while ((line = reader.readLine()) != null) {
+                    if (!headerSkipped) {
                         headerSkipped = true;
                         continue;
                     }
                     String[] fileContent = line.split("\t");
 
-                    if (fileContent.length == 7 && fileContent[0].equals(getIdNumber())){
+                    // Check if the line contains information for the requested employee
+                    if (fileContent.length == 7 && fileContent[0].equals(getIdNumber())) {
                         taxRegistered = true;
                     }
                 }
-
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error has occurred.");
         }
         return taxRegistered;
     }
+
+    // Getter and setter methods for utilized attributes
+
     public double getRegularPay() {
         return regularPay;
     }
